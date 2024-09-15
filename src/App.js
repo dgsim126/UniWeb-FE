6fa5e50db996d8179fb26cb719396a47d9c3b2c2
Hyeongseok Kim <king9833@korea.ac.kr>
2024-09-15 20:31:05 +0900

    initial frame

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(false);
+  useEffect(() => {
+    console.log(authenticate);
+  }, [authenticate]) // authenticate값이 바뀔 때 마다 실행.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar authenticate={authenticate} setAuthenticate={setAuthenticate} />
+      <Routes>
+        <Route path="/" element={<ProductAll/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Login/>}/>
+
+        <Route path="/:id" element={<PrivateRoute authenticate={authenticate} />}/>
+        <Route path="/play" element={<PrivateRoute authenticate={authenticate} />}/>
+        <Route path="/create" element={<PrivateRoute authenticate={authenticate} />}/>
+        <Route path="/my" element={<PrivateRoute authenticate={authenticate} />}/>
+      </Routes>
+      <Footer />
     </div>
   );
 }
