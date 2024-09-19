32844cf2d03434326054727be661459a6001025d
Gogumi33 <135415213+Gogumi33@users.noreply.github.com>
2024-09-19 16:17:55 +0900

    conditonal rendering

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ProductDetail from '../page/ProductDetail'
 import { Navigate } from 'react-router-dom'
 
+// 조건부 렌더링
 const PrivateRoute = ({authenticate}) => {
   return authenticate===true ? <ProductDetail /> : <Navigate to="/login" />;
 }
