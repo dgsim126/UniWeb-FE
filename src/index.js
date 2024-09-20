74b4eac726d63a44121578ae57c7d2440e1ae0d6
Gogumi33 <135415213+Gogumi33@users.noreply.github.com>
2024-09-21 01:36:16 +0900

    atoms

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <RecoilRoot>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </RecoilRoot>
 );
 
 // If you want to start measuring performance in your app, pass a function
