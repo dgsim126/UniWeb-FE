03c43f8e270d80073ff1ec3c5bb0da0f459978a5
Gogumi33 <135415213+Gogumi33@users.noreply.github.com>
2024-11-05 18:30:32 +0900

    ProtectedRoute

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,6 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 // ProtectedRoute 컴포넌트: 로그인이 필요할 때 사용하는 보호된 라우트
+// 마이페이지, 게임생성하기, 게임 플레이하기 등
 const ProtectedRoute = ({ isLoggedIn, children }) => {
   if (!isLoggedIn) {
     return <Navigate to="/login" replace />;
