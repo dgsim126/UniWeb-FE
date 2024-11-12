ef3d2161a950ecc31278166efecbf53096dfd4a0
Gogumi33 <135415213+Gogumi33@users.noreply.github.com>
2024-11-12 21:20:16 +0900

    backend deploy test

--- src/APIs/axiosInstance.js
+++ src/APIs/axiosInstance.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/'; // 로컬 테스트시 사용
+// const API_URL = 'http://localhost:8080/'; // 로컬 테스트시 사용
 // const API_URL = 'https://uniwebbackend.duckdns.org';
 
 // const API_URL = 'https://uniwebb.freeddns.org'; // 바뀐 백엔드 URL[AWS]
+const API_URL = 'https://uniweb-backend.duckdns.org'; // 🌟 형석's 백엔드 배포주소
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
